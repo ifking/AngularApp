8c1a34e5d3af07b008c2820c9b2bb18cf9049cd2
Jason Mackey <jmackey645@gmail.com>
2017-10-30 18:02:28 -0400

    update App.js
    Include a test_objects array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module("testApp",[]);
+(function() {
+  angular.module("testApp",[])
+  .controller("testCtrl", function() {
+    this.tests = test_object;
+  });
+
+  var test_object = [{
+    name: "test_1"
+  }, {
+    name: "test_2"
+  }, {
+    name: "test_3"
+  }];
+})
